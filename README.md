6a2ba02da06da2e76e4aa47e17018e4aa43aadff
Sheng <s.tsui@outlook.com>
2020-10-20 22:21:44 +1300

    books displaying

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
 library
+Not relevant to this problem
+But you need to check your 'read' status on your books
+The form will return true/false
+but in your two sample books you state 'read'
+on lines 3/4
+
+issue:
+So the problem is that something is reloading the page
+The code itself is working, but the submit button is reloading everything
+I put a console.log of the library in before the renderBook(book) line, and it showed three books in the library, and as you mentioned it appears very briefly on the page
+but then everything reloads and the book is no longer in the library
+meaning the program is resetting
+OK
+@BubbleTearju The problem is that the buttons are inside of the form tag
+So the default response is for the page to reload
+Move the form tag in front of the buttons
+The problem is the form now doesn't disappear :smile:
+WAIT
+I remember something about this
+something like 'prevent default'
+
+const submitButton = document.querySelector("#submit-button");
+submitButton.addEventListener("click", (event) => {
+    addBookToLibrary()
+    toggleModal()
+  event.preventDefault()
+
+});
+
+you'll also need to clear the values in there, since we're preventing it from doing so using that preventdefault
+
+
+
+so whenever the page is reloaded, the library becomes empty and then two books are put in, and then they're rendered
+So a page reload will only ever show two books
+
+
+
+Yes, the submit button has a default property as part of html
+which is to clear the form
+and reload the page
+
+It seems like any button will do the reset
+[9:36 PM]
+submit: The button submits the form data to the server. This is the default if the attribute is not specified, or if the attribute is dynamically changed to an empty or invalid value.
--- script.js
+++ script.js
@@ -1,28 +1,80 @@
-// let myLibrary = [];
+let myLibrary = [];
 
-// function Book(title, author, pages, read) {
-//     this.title = title;
-//     this.author = author;
-//     this.pages = pages;
-//     this.read = read;
-//     this.info = function() {
-//         return `${this.title} by ${this.author}, ${this.pages}, ${this.read}`;
-//     }
-// }
+let book1 = new Book("To Kill A Mockingbird", "Harper Lee", "296", read);
+let book2 = new Book("The Hobbit", "J. R. R. Tolkien", "310", read);
+myLibrary.push(book1);
+myLibrary.push(book2);
 
-// function addBookToLibrary() {
-//     let book = new Book();
-//     myLibrary.push(book);
-//     //renderBook();
-// }
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function() {
+        return `${this.title} by ${this.author}, ${this.pages}, ${this.read}`;
+    }
+}
 
-// // function renderBook() {
+function addBookToLibrary() {
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const pages = document.getElementById('pages').value;
+    const read = document.getElementById('read').checked;
 
-// // }
+    const form = document.getElementById('form');
+    
+    let book = new Book(title, author, pages, read);
+    myLibrary.push(book);
 
+    console.log(myLibrary);
+
+    renderBook(book);
+}
+
+function renderLibrary() {
+    for (let i=0; i<myLibrary.length; i++){
+        renderBook(myLibrary[i]);
+    }
+}
+
+function renderBook(book) {
+    const library = document.querySelector(".bookSpace");
+    const bookDiv = document.createElement("div");
+    const titleDiv = document.createElement("div");
+    const authorDiv = document.createElement("div");
+    const pagesDiv = document.createElement("div");
+
+    const readBtn = document.createElement("button");
+    const removeBtn = document.createElement("button");
+
+    bookDiv.classList.add("book-div");
+    // bookDiv.setAttribute("id", myLibrary.indexOf(book));
+    library.appendChild(bookDiv);
+
+    titleDiv.classList.add("title-div");
+    titleDiv.textContent = book.title;
+    bookDiv.appendChild(titleDiv);
+
+    authorDiv.classList.add("author-div");
+    authorDiv.textContent = book.author;
+    bookDiv.appendChild(authorDiv);
+
+    pagesDiv.classList.add("pages-div");
+    pagesDiv.textContent = book.pages;
+    bookDiv.appendChild(pagesDiv);
+
+    readBtn.classList.add("read-btn");
+    readBtn.textContent = "Read?";
+    bookDiv.appendChild(readBtn);
+
+    removeBtn.classList.add("remove-btn");
+    removeBtn.textContent = "Remove";
+    bookDiv.appendChild(removeBtn);
+}
 
 // //https://www.w3schools.com/howto/howto_css_modals.asp
 // //https://sabe.io/tutorials/how-to-create-modal-popup-box
+
 const modal = document.querySelector(".modal");
 
 const trigger = document.querySelector(".trigger");
@@ -34,12 +86,18 @@ closeButton.addEventListener("click", toggleModal);
 const cancelButton = document.querySelector("#cancel-button");
 cancelButton.addEventListener("click", toggleModal);
 
+// const submitButton = document.querySelector("#submit-button");
+// submitButton.addEventListener("click", addBookToLibrary); //old code
+
+const submitButton = document.querySelector("#submit-button");
+submitButton.addEventListener("click", (event) => {
+    addBookToLibrary();
+    toggleModal();
+    event.preventDefault();
+});
+
 function toggleModal() {
-    modal.classList.toggle("show-modal");
+    modal.classList.toggle("show-modal"); //toggles between .modal class and .show-modal class
 }
 
-
-
-
-
-
+renderLibrary();
